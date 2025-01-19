7a8db1b8c3fceeef0247ccc5f24b910085903ed8
scofieldacreep <chizhang2048@gmail.com>
2025-01-19 20:02:06 +0800

    hello

---
+++ FirstApp/ByeView.swift
@@ -0,0 +1,18 @@
+//
+//  ByeView.swift
+//  FirstApp
+//
+//  Created by Chi Zhang on 1/19/25.
+//
+
+import SwiftUI
+
+struct ByeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ByeView()
+}
